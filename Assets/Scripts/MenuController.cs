6c18ff59431a10897ea98db71099b1471fad7c95
Ivi200505 <Ivanrequenapuig@gmail.com>
2025-02-06 14:56:45 +0100

    Menu

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
+
+
+
+
 
 public class MenuController : MonoBehaviour
 {
-   [Header("Levels to Load")]
+    [Header("Volume Setting")]
+    [SerializeField] private TMP_Text volumeTextValue = null;
+    [SerializeField] private Slider volumeSlider = null;
+
+    [SerializeField] private GameObject confirmationPrompt = null;
+
+
+    [Header("Levels to Load")]
    public string _newGameLevel;
    private string levelToLoad;
    [SerializeField] private GameObject noSavedGameDialog = null;
@@ -31,4 +43,31 @@ else{
 public void ExitButton(){
     Application.Quit();
 }
+
+public void SetVolume(float volume)
+    {
+       AudioListener.volume = volume;
+       volumeTextValue.text = volume.ToString("0.0"); 
+    }
+    public void VolumeApply()
+    {
+        PlayerPrefs.SetFloat("masterVolume", AudioListener.volume);
+        StartCoroutine(ConfirmationBox());
+        // Show prompt
+    }
+
+    public IEnumerator ConfirmationBox()
+    {
+        confirmationPrompt.SetActive(true);
+        yield return new WaitForSeconds(2);
+        confirmationPrompt.SetActive(false);
+
+    }
+
+
+
+
+
+
+
 }
