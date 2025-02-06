af2088904408f382fbdc9b5cb2ca410863c36b2e
Mariio8005 <mariiobenediictomoreno@gmail.com>
2025-02-06 14:13:59 +0100

    pasillo blanco

--- Assets/Brick Project Studio/_BPS Basic Assets/Common/Scripts and Animations/First Person Player/PlayerMovement.cs
+++ Assets/Brick Project Studio/_BPS Basic Assets/Common/Scripts and Animations/First Person Player/PlayerMovement.cs
@@ -3,35 +3,54 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace SojaExiles
-
 {
     public class PlayerMovement : MonoBehaviour
     {
-
         public CharacterController controller;
 
         public float speed = 5f;
         public float gravity = -15f;
 
-        Vector3 velocity;
+        public Transform playerCamera; // Referencia a la cámara del jugador
+        public float mouseSensitivity = 100f;
 
-        bool isGrounded;
+        private Vector3 velocity;
+        private bool isGrounded;
+        private float xRotation = 0f;
 
-        // Update is called once per frame
+        void Start()
+        {
+            Cursor.lockState = CursorLockMode.Locked; // Bloquea el cursor en el centro de la pantalla
+        }
+
         void Update()
         {
+            MovePlayer();
+            RotateCamera();
+        }
 
+        void MovePlayer()
+        {
             float x = Input.GetAxis("Horizontal");
             float z = Input.GetAxis("Vertical");
 
             Vector3 move = transform.right * x + transform.forward * z;
-
             controller.Move(move * speed * Time.deltaTime);
 
             velocity.y += gravity * Time.deltaTime;
-
             controller.Move(velocity * Time.deltaTime);
+        }
 
+        void RotateCamera()
+        {
+            float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+            float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+            xRotation -= mouseY;
+            xRotation = Mathf.Clamp(xRotation, -90f, 90f); // Evita que la cámara gire demasiado arriba o abajo
+
+            playerCamera.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+            transform.Rotate(Vector3.up * mouseX);
         }
     }
 }
